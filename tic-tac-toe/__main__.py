917158a1de88769d28fdacb3f06b4c46a6cb9cc0
Shane Satterfield <dustyplant@gmail.com>
2015-08-18 22:07:27 -0500

    Now primitively renders to the screen and you can play through mouse clicks.

--- tic-tac-toe/__main__.py
+++ tic-tac-toe/__main__.py
@@ -1,20 +1,10 @@
 from __future__ import print_function, division
 from game import Game
+import pyglet
 
 def main():
     game = Game()
-
-    first_player = True
-    for n in range(9):
-        game.render()
-        row, column = int(input()), int(input())
-        game.make_move( row, column, first_player )
-
-        winner = game.check_win()
-        if winner != 0:
-            print('Player {} won!'.format(winner))
-
-        first_player = not first_player
+    pyglet.app.run()
 
 if __name__ == '__main__':
     main()
--- tic-tac-toe/game.py
+++ tic-tac-toe/game.py
@@ -1,33 +1,56 @@
-from __future__ import print_function
-import random
+from __future__ import print_function, division
+import pyglet, random
 
-class Game:
+class Game( pyglet.window.Window ):
     def __init__( self ):
         self.restart()
+        super(Game, self).__init__( self.grid_height, self.grid_height )
+        self.images = [ pyglet.image.load('res/cross.png'), pyglet.image.load('res/circle.png') ]
+
+    def on_draw( self ):
+        pyglet.window.Window.flip( self )
+        self.clear()
+        for row in range(len(self.grid)):
+            for column in range(len(self.grid[row])):
+                if self.grid[row][column] != 0:
+                    self.images[self.grid[row][column]-1].blit( column*self.tile_height, (2-row)*self.tile_width )
+
+    def on_mouse_release( self, x, y, button, modifiers ):
+        if button == pyglet.window.mouse.LEFT:
+            self.make_move( (self.grid_height-y) // self.tile_height, x // self.tile_width )
+
+            winner = self.check_win()
+            if winner != 0:
+                print('Player {} won!'.format(winner))
+                super(Game, self).close()
 
     def restart( self ):
+        self.first_player = True
+        self.tile_width, self.tile_height = 32, 32
+        self.grid_width, self.grid_height = self.tile_width * 3, self.tile_height * 3
         self.grid = [ [ 0 for _ in range(3) ] for _ in range(3) ]
 
-    def make_move( self, row, column, first_player ):
+    def make_move( self, row, column ):
         if row < 0 or row >= len(self.grid) or column < 0 or column >= len(self.grid[0]):
             raise IndexError('The row or column you specified is not within the correct range.')
 
         if self.grid[row][column] is not 0:
             raise ValueError('The space you selected is already occupied.')
 
-        self.grid[row][column] = 1 if first_player else 2
+        self.grid[row][column] = 1 if self.first_player else 2
+        self.first_player = not self.first_player
 
     def check_win( self ):
         grid = list(self.grid)
         for _ in range(2):
             for i in range(3):
-                if grid[i][0] == grid[i][1] == grid[i][2]:
+                if grid[i][0] == grid[i][1] and grid[i][1] == grid[i][2]:
                     return grid[i][0]
             grid = list(zip(*grid))
             grid = list(map(list, grid))
 
-        if grid[0][0] == grid[1][1] == grid[2][2] or \
-           grid[2][0] == grid[1][1] == grid[0][2]:
+        if grid[0][0] == grid[1][1] and grid[1][1] == grid[2][2] or \
+           grid[2][0] == grid[1][1] and grid[1][1] == grid[0][2]:
             return grid[1][1]
 
         return 0
